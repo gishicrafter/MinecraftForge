ea7db08c0ac16309c45e58d593ff3ff2c4a4ce75
gishicrafter <gishicrafter@gmail.com>
2013-01-18 22:33:00 +0900

    added defaultItem which will be used when liquid not found in the dictionary.

--- common/net/minecraftforge/crafting/tags/ItemFilledContainer.java
+++ common/net/minecraftforge/crafting/tags/ItemFilledContainer.java
@@ -2,6 +2,8 @@ package net.minecraftforge.crafting.tags;
 
 import java.util.ArrayList;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.crafting.ICraftingMaterial;
 import net.minecraftforge.liquids.LiquidContainerData;
@@ -16,25 +18,86 @@ public class ItemFilledContainer implements ICraftingMaterial {
     protected int amount;
     protected boolean exact;
     protected ItemStack[] excludes;
+    protected ItemStack defaultItem;
 
     public ItemFilledContainer(String name, int amount)
     {
-        this(name, amount, false);
+        this(name, amount, false, (ItemStack) null);
+    }
+
+    public ItemFilledContainer(String name, int amount, Block defaultItem)
+    {
+        this(name, amount, false, new ItemStack(defaultItem));
+    }
+
+    public ItemFilledContainer(String name, int amount, Item defaultItem)
+    {
+        this(name, amount, false, new ItemStack(defaultItem));
+    }
+
+    public ItemFilledContainer(String name, int amount, ItemStack defaultItem)
+    {
+        this(name, amount, false, defaultItem);
     }
 
     public ItemFilledContainer(String name, int amount, boolean exact)
     {
-        this(name, amount, exact, null);
+        this(name, amount, exact, (ItemStack) null);
+    }
+
+    public ItemFilledContainer(String name, int amount, boolean exact, Block defaultItem)
+    {
+        this(name, amount, exact, new ItemStack(defaultItem));
+    }
+
+    public ItemFilledContainer(String name, int amount, boolean exact, Item defaultItem)
+    {
+        this(name, amount, exact, new ItemStack(defaultItem));
+    }
+
+    public ItemFilledContainer(String name, int amount, boolean exact, ItemStack defaultItem)
+    {
+        this(name, amount, exact, null, defaultItem);
     }
 
     public ItemFilledContainer(String name, int amount, ItemStack[] excludes)
     {
-        this(name, amount, false, excludes);
+        this(name, amount, excludes, (ItemStack) null);
+    }
+
+    public ItemFilledContainer(String name, int amount, ItemStack[] excludes, Block defaultItem)
+    {
+        this(name, amount, excludes, new ItemStack(defaultItem));
+    }
+
+    public ItemFilledContainer(String name, int amount, ItemStack[] excludes, Item defaultItem)
+    {
+        this(name, amount, excludes, new ItemStack(defaultItem));
+    }
+
+    public ItemFilledContainer(String name, int amount, ItemStack[] excludes, ItemStack defaultItem)
+    {
+        this(name, amount, false, excludes, defaultItem);
     }
 
     public ItemFilledContainer(String name, int amount, boolean exact, ItemStack[] excludes)
     {
-        this(null, name, amount, exact, excludes);
+        this(name, amount, exact, excludes, (ItemStack) null);
+    }
+
+    public ItemFilledContainer(String name, int amount, boolean exact, ItemStack[] excludes, Block defaultItem)
+    {
+        this(name, amount, exact, excludes, new ItemStack(defaultItem));
+    }
+
+    public ItemFilledContainer(String name, int amount, boolean exact, ItemStack[] excludes, Item defaultItem)
+    {
+        this(name, amount, exact, excludes, new ItemStack(defaultItem));
+    }
+
+    public ItemFilledContainer(String name, int amount, boolean exact, ItemStack[] excludes, ItemStack defaultItem)
+    {
+        this(null, name, amount, exact, excludes, defaultItem);
     }
 
     public ItemFilledContainer(LiquidStack liquid)
@@ -54,22 +117,28 @@ public class ItemFilledContainer implements ICraftingMaterial {
 
     public ItemFilledContainer(LiquidStack liquid, boolean exact, ItemStack[] excludes)
     {
-        this(liquid, null, liquid.amount, exact, excludes);
+        this(liquid, null, liquid.amount, exact, excludes, null);
     }
 
-    protected ItemFilledContainer(LiquidStack liquid, String name, int amount, boolean exact, ItemStack[] excludes)
+    protected ItemFilledContainer(LiquidStack liquid, String name, int amount, boolean exact, ItemStack[] excludes, ItemStack defaultItem)
     {
-        this.liquid = (liquid != null ? liquid.copy() : null);
+        this.liquid = liquid != null ? liquid.copy() : null;
         this.name = name;
         this.amount = amount;
         this.exact = exact;
-        this.excludes = new ItemStack[excludes.length];
-        for(int i = 0; i < excludes.length; ++i)
-        {
-            this.excludes[i] = excludes[i].copy();
+        if(excludes != null){
+            this.excludes = new ItemStack[excludes.length];
+            for(int i = 0; i < excludes.length; ++i)
+            {
+                this.excludes[i] = excludes[i].copy();
+            }
+        }else{
+            this.excludes = null;
         }
+        this.defaultItem = defaultItem!=null?defaultItem.copy():null;
     }
 
+    @Override
     public boolean isItemEqual(ItemStack other)
     {
         if (this.liquid == null && name != null)
@@ -79,14 +148,15 @@ public class ItemFilledContainer implements ICraftingMaterial {
 
         if (this.liquid == null)
         {
-            return false;
+            return defaultItem != null && defaultItem.isItemEqual(other);
         }
 
         LiquidStack liquid = LiquidContainerRegistry.getLiquidForFilledItem(other);
 
-        return liquid != null && ((exact && liquid.amount == this.liquid.amount) || (!exact && liquid.amount >= this.liquid.amount));
+        return liquid != null && (exact && liquid.amount == this.liquid.amount || !exact && liquid.amount >= this.liquid.amount);
     }
 
+    @Override
     public ArrayList<ItemStack> getItems()
     {
         ArrayList<ItemStack> result = new ArrayList<ItemStack>();
@@ -98,25 +168,32 @@ public class ItemFilledContainer implements ICraftingMaterial {
 
         if (liquid == null)
         {
+            if(defaultItem != null)
+            {
+                result.add(defaultItem);
+            }
             return result;
         }
 
         for (LiquidContainerData data : LiquidContainerRegistry.getRegisteredLiquidContainerData())
         {
             if (data.stillLiquid.isLiquidEqual(liquid)
-                    && ((exact && data.stillLiquid.amount == liquid.amount) || (!exact && data.stillLiquid.amount >= liquid.amount)))
+                    && (exact && data.stillLiquid.amount == liquid.amount || !exact && data.stillLiquid.amount >= liquid.amount))
             {
                 if (excludes != null)
                 {
                     boolean include = true;;
                     for (ItemStack item : excludes)
                     {
-                        if ((item.getItemDamage() == -1 && item.itemID == data.filled.itemID) || item.isItemEqual(data.filled))
+                        if (item.getItemDamage() == -1 && item.itemID == data.filled.itemID || item.isItemEqual(data.filled))
                         {
                             include = false;
                         }
                     }
-                    if (include) result.add(data.filled);
+                    if (include)
+                    {
+                        result.add(data.filled);
+                    }
                 }
                 else
                 {
